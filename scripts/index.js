dd30d7ffeb8e3f123d816e64b95323e2d79ac637
Natalia Luna <lunap.natalia@gmail.com>
2023-06-01 23:20:04 +0100

    Add menu animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 function goToPage(pageName) {
     switch (pageName) {
         case "home":
-            window.location.href = "index.html";
+            window.location.href = "../index.html";
+            hideMenu();
             break;
         case "work":
-            window.location.href = "../index.html#mywork";
+            window.location.href = "index.html#work";
             break;
         case "virtualReality":
             window.location.href = "WorkPages/virtualReality.html";
@@ -31,33 +32,18 @@ function goToPage(pageName) {
 
 }
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() { myFunction(); };
-
-// Get the header
-var header = document.getElementById("menuBtn");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
-
-function showMenu() {
-    const showPanel = [
-        { width: "100%" }
-    ];
-
-    const timing = {
-        duration: 2000,
-        iterations: 1,
-    };
+function hideMenu() {
+    var menuButton = document.getElementById("menuBtn");
     var menuPanel = document.getElementById("menu-panel");
-    menuPanel.animate(showPanel);
+    menuButton.classList.toggle("active");
+    menuPanel.classList.toggle("active");
+    // element.classList.remove(className);
 }
+
+$(document).ready(function() {
+    $('.menu-button').click(function() {
+        $('.menu-button').toggleClass('active');
+        $('.menu-panel').toggleClass('active');
+        // $('body').toggleClass('stop-scrolling');
+    });
+});
