29147e953eb791c74fc27e15977ccce71d2cb809
Natalia Luna <lunap.natalia@gmail.com>
2023-06-05 19:05:41 +0100

    remove menu and make website resizable

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,37 @@ function goToPage(pageName) {
     switch (pageName) {
         case "home":
             window.location.href = "../index.html";
-            hideMenu();
+            // hideMenu();
+            break;
+        case "homeFromHome":
+            window.location.href = "index.html";
+            // hideMenu();
             break;
         case "work":
             window.location.href = "index.html#work";
             break;
         case "virtualReality":
             window.location.href = "WorkPages/virtualReality.html";
+            // hideMenu();
             break;
         case "webDev":
             window.location.href = "WorkPages/webDev.html";
+            // hideMenu();
             break;
         case "videoEdition":
             window.location.href = "WorkPages/videoEdition.html";
+            // hideMenu();
             break;
         case "animation":
             window.location.href = "WorkPages/animation.html";
+            // hideMenu();
             break;
-            // case "desktopApp":
-            //     window.location.href = "WorkPages/desktopApp.html";
-            //     break;
+        case "illustration":
+            window.location.href = "WorkPages/illustration.html";
+            break;
         case "uxui":
             window.location.href = "WorkPages/uxui.html";
+            // hideMenu();
             break;
         default:
             window.location.href = "../index.html";
